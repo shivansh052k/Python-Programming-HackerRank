37bdde1c90edf75732f622c6cb9f21b180d707d1
Shivansh Gupta <shivanshgupta323@gmail.com>
2020-07-22 16:20:40 +0530

    Update Question2.py

--- Question2.py
+++ Question2.py
@@ -17,3 +17,26 @@ Output Format
 Print Weird if the number is weird. Otherwise, print Not Weird.
 
 """
+
+#Solution:
+
+#!/bin/python
+
+import math
+import os
+import random
+import re
+import sys
+
+
+n = int(input(""))
+
+if n%2 != 0:
+    print("Weird")
+elif (n%2 == 0) and (n>=2 and n<=5):
+    print("Not Weird")
+elif (n%2 == 0) and (n>=6 and n<=20):
+    print("Weird")
+elif (n%2 == 0) and (n>20):
+    print("Not Weird")
+    
